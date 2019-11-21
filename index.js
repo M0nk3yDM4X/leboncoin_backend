4442e0816da7ec9f7edc0e5546fcccaed3f1aacc
Your Name <you@example.com>
2019-11-21 13:33:45 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const formidableMiddleware = require("express-formidable");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ message: "hello world" });
+});
+
+// test
+
+app.listen(4000, () => {
+  console.log("server started");
+});
